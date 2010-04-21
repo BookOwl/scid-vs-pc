c91c2ffd167d16b78fbe67c4035713b522915ff0
stevenaaus <stevenaaus@b49c361e-7f30-453a-be38-6c4a2cbfac04>
2010-04-21 07:38:04 +0000

    player info tweaks

--- tcl/tools/pinfo.tcl
+++ tcl/tools/pinfo.tcl
@@ -1,6 +1,6 @@
-
-####################
-# Player Info window
+###
+### pinfo.tcl # Player Info window
+###
 
 set playerInfoName ""
 
@@ -17,41 +17,46 @@ proc playerInfo {{player ""}} {
     wm title $w "Scid: [tr ToolsPInfo]"
     wm minsize $w 40 5
     pack [frame $w.b2] -side bottom -fill x
-    pack [frame $w.b] -side bottom -fill x
+    pack [frame $w.b] -side bottom
     button $w.b.graph -text [tr ToolsRating] \
-      -command {::tools::graphs::rating::Refresh player $playerInfoName}
+      -command {::tools::graphs::rating::Refresh player $playerInfoName} -width 10
     button $w.b.edit -text $::tr(PInfoEditRatings) -command {
       makeNameEditor
       setNameEditorType rating
       set editName $playerInfoName
       set editNameSelect crosstable
-    }
-    button $w.b2.report -text [tr ToolsPlayerReport] \
-      -command {::preport::preportDlg $playerInfoName}
-    dialogbutton $w.b2.help -textvar ::tr(Help) -command {helpWindow PInfo}
-    dialogbutton $w.b2.update -textvar ::tr(Update) -command {playerInfo $playerInfoName}
-    dialogbutton $w.b2.close -textvar ::tr(Close) -command "focus .; destroy $w"
-    packbuttons right $w.b2.close $w.b2.update $w.b2.help
-    packbuttons left $w.b.graph $w.b.edit
-    packbuttons left $w.b2.report
+    } -width 10
+    button $w.b.report -text [tr ToolsPlayerReport] \
+      -command {::preport::preportDlg $playerInfoName} -width 10
+    button $w.b.update -textvar ::tr(Update) -command {playerInfo $playerInfoName} -width 10
+    dialogbutton $w.b2.help -textvar ::tr(Help) -command {helpWindow PInfo} -width 10
+    dialogbutton $w.b2.close -textvar ::tr(Close) -command "focus .; destroy $w" -width 10
 
-    autoscrollframe $w.frame text $w.text -font font_Regular -background white \
-      -width $::winWidth($w) -height $::winHeight($w) -setgrid 1 -wrap none
+    pack $w.b.report -padx 5 -pady 5 -side left
+    pack $w.b.graph  -padx 5 -pady 5 -side left
+    pack $w.b.edit   -padx 5 -pady 5 -side left
+    pack $w.b.update -padx 5 -pady 5 -side right
+    pack $w.b2.close -padx 10 -pady 10 -side right -anchor e
+    pack $w.b2.help  -padx 10 -pady 10 -side right -anchor e
+
+    autoscrollframe $w.frame text $w.text -font font_Regular -wrap none
+
     #scrollbar $w.ybar -command "$w.text yview"
     #pack $w.ybar -side right -fill y
-    #text $w.text -font font_Regular -background white \
+    #text $w.text -font font_Regular \
     #  -width $::winWidth($w) -height $::winHeight($w) \
     #  -setgrid 1 -wrap none -yscrollcommand "$w.ybar set"
-    label $w.photo -background white
     #pack $w.text -side top -fill both -expand yes
+
+    label $w.photo 
     pack $w.frame -side top -fill both -expand yes
-    bind $w <Escape> "focus .; destroy $w"
     ::htext::init $w.text
     ::htext::updateRate $w.text 0
     bind $w <Escape> "focus .; destroy $w"
     bind $w <F1> {helpWindow PInfo}
     bind $w <Configure> "recordWinSize $w"
     standardShortcuts $w
+    setWinSize $w
   }
   set player [trimEngineName $player]
   if {[info exists ::photo($player)]} {
@@ -61,12 +66,20 @@ proc playerInfo {{player ""}} {
   } else {
     place forget $w.photo
   }
+
+  ### update text window contents
+
   $w.text configure -state normal
   $w.text delete 1.0 end
   ::htext::display $w.text $pinfo
   $w.text configure -state disabled
+
   ::windows::gamelist::Refresh
   ::maint::Refresh
+
   #raiseWin $w
 }
 
+###
+### end of pinfo.tcl
+###
