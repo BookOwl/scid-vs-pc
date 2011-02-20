00124ef3e35c71c37b88b307ee1ee579f25c25a4
stevenaaus <stevenaaus@b49c361e-7f30-453a-be38-6c4a2cbfac04>
2011-02-20 06:30:08 +0000

    Enable/disable {Paste Variation} menu the same as [tr EditAdd]

--- tcl/main.tcl
+++ tcl/main.tcl
@@ -777,10 +777,12 @@ proc updateBoard2 {} {
   # Cannot add a variation to an empty line:
   if {[sc_pos isAt vstart]  &&  [sc_pos isAt vend]} {
     .menu.edit entryconfig [tr EditAdd] -state disabled
+    .menu.edit entryconfig {Paste Variation}  -state disabled
     .button.addVar configure -state disabled
     bind . <Control-a> {}
   } else {
     .menu.edit entryconfig [tr EditAdd] -state normal
+    .menu.edit entryconfig {Paste Variation}  -state normal
     .button.addVar configure -state normal
     bind . <Control-a> {sc_var create; updateBoard -pgn}
   }
