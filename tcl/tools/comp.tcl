1812112ea79979bfeed89b0dc3591faef6196c6e
stevenaaus <stevenaaus@b49c361e-7f30-453a-be38-6c4a2cbfac04>
2011-10-24 09:35:59 +0000

    Ooops. 50 moves means 100 half moves.

--- tcl/tools/comp.tcl
+++ tcl/tools/comp.tcl
@@ -708,7 +708,7 @@ proc compNM {n m k} {
 	set fen [sc_pos fen]
 	set f [lindex $fen 0]
 	lappend comp(fen) $f
-	if {[llength [lsearch -all $comp(fen) $f]] > 2 || [lindex $fen 4] > 49} {
+	if {[llength [lsearch -all $comp(fen) $f]] > 2 || [lindex $fen 4] > 99} {
 	  sc_game tags set -result =
 	  ### draw
 	  puts_ Draw
