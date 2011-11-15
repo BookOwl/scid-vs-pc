38b148704b11ab87e753e5fd3e1bd511841d669c
stevenaaus <stevenaaus@b49c361e-7f30-453a-be38-6c4a2cbfac04>
2011-11-15 09:50:56 +0000

    Version bump for 4.6

--- src/common.h
+++ src/common.h
@@ -62,8 +62,8 @@ typedef unsigned short versionT;
 const versionT SCID_VERSION = 400;     // Current file format version = 3.0
 const versionT SCID_OLDEST_VERSION = 400; // Oldest compatible format: 3.0
 
-const char SCID_VERSION_STRING[] = "4.5";     // Current Scid version
-const char SCID_VERSION_DATE[] = "Aug 10, 2011";
+const char SCID_VERSION_STRING[] = "4.6";     // Current version
+const char SCID_VERSION_DATE[] = "Nov 20, 2011";
 const char SCID_WEBSITE[] = "http://scidvspc.sourceforge.net/";
 
 const char TREEFILE_SUFFIX[] = ".stc";
