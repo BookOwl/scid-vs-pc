1337466fdab4fcc9c11575d6eaad8787892f58c4
stevenaaus <stevenaaus@b49c361e-7f30-453a-be38-6c4a2cbfac04>
2013-11-24 02:40:44 +0000

    4.11 version bump.

--- tcl/start.tcl
+++ tcl/start.tcl
@@ -13,8 +13,8 @@
 exec `dirname $0`/tkscid "$0" "$@"
 
 # Alter the version if any patches have been made to the Tcl code only:
-set scidVersion 4.10
-set scidVersionDate {Aug 25, 2013}
+set scidVersion 4.11
+set scidVersionDate {Dec 1, 2013}
 
 # Set to 0 before releasing, so some alpha-state code is not included
 # Set to 1 to have access to all code
