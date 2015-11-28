e33f7016e8d7b185a2fb839255edbca6b44ce5f4
stevenaaus <stevenaaus@b49c361e-7f30-453a-be38-6c4a2cbfac04>
2015-11-28 13:10:55 +0000

    When doing a 'Save Layout', automatically 'Save Options' too.

--- tcl/menus.tcl
+++ tcl/menus.tcl
@@ -830,6 +830,7 @@ if {$::docking::USE_DOCKING} {
   $m add command -label OptionsWindowsSaveLayout -command {
     ::docking::layout_save 1
     set autoLoadLayout 1
+    .menu.options invoke [tr OptionsSave]
   }
 }
 
